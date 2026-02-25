ea7545049353a1323ebf5eac7bf0bb628427cbf2
Chanathinart Ruangvittayanon <cntnruang@gmail.com>
2026-02-25 18:38:05 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,13 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',          // ต้องเป็น myuser ไม่ใช่ postgres
+  user: 'postgres',        
   host: 'db',              // รัน Backend ข้างใน Docker ใช้ชื่อ service ใน docker-compose
   database: 'helpdesk_db', // ชื่อ DB ที่สร้างใน Docker
   password: 'password123',  // รหัสผ่านในไฟล์ docker-compose
   port: 5432,
 });
 
-// ลองเพิ่มโค้ดเช็คการเชื่อมต่อตรงนี้
 pool.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack);
